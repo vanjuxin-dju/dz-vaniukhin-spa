ecb72267567dde90646c1d67f5d38bb540958433
vanjuxin-dju <sokol.vdu@gmail.com>
2022-11-18 15:47:59 +0300

    added linkedin link to Contact section

--- src/Contact.js
+++ src/Contact.js
@@ -8,6 +8,7 @@ class Contact extends Component {
             <p>Ask any questions: <a href="mailto:sokol.vdu@gmail.com">sokol.vdu@gmail.com</a> <small className="text-muted">(preferable option)</small></p>
             <p>Phone: <a href="tel:+375295831788">+375295831788</a></p>
             <p>Skype: <a href="skype:sokol_vdu?chat">sokol_vdu</a></p>
+            <p>LinkedIn: <a href="https://www.linkedin.com/in/dzmitry-vaniukhin-5618a1192/" target="_blank">Dzmitry Vaniukhin</a></p>
           </div>
         );
     }
