1f65e992821c7a58d5819a677720368c0bcd2405
vanjuxin-dju <sokol.vdu@gmail.com>
2022-11-18 17:28:09 +0300

    updated dependencies, added webpack config for production

---
+++ webpack.config.prod.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "public", "dist"),
+    filename: "bundle.js"
+  }
+};
