36a9f1cbe286e7e943adf32a25ea012fa73a8298
vanjuxin-dju <sokol.vdu@gmail.com>
2023-01-19 12:36:07 +0300

    updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,77 +3,83 @@ const webpack = require("webpack");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: {
-    "bundle": "./src/index.js",
-    "clientlibs": "./src/clientlibs.js"
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
-      },
-      {
-        test: /\.(le|c)ss$/,
-        exclude: /(node_modules|bower_components)/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          {
-            loader: "css-loader"
-          },
-          {
-            loader: "less-loader"
-          }
-        ]
-      },
-      {
-        test: /\.s[ac]ss$/,
-        exclude: /(node_modules|bower_components)/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          {
-            loader: "css-loader"
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [
-                  [
-                    "autoprefixer"
-                  ],
-                ],
-              }
+    entry: {
+        "bundle": "./src/index.js",
+        "clientlibs": "./src/clientlibs.js"
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: { presets: ["@babel/env"] }
+            },
+            {
+                test: /\.(le|c)ss$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "less-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "autoprefixer"
+                                    ],
+                                ],
+                            }
+                        }
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ]
             }
-          },
-          {
-            loader: "sass-loader"
-          }
         ]
-      }
+    },
+    resolve: { extensions: ["*", ".js", ".jsx", ".css", ".less"] },
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/dist/",
+        filename: "[name].js"
+    },
+    devServer: {
+      static: {
+          directory: path.join(__dirname, "public")
+      },
+      port: 3000,
+      devMiddleware: {
+          index: true,
+          publicPath: "http://localhost:3000/dist/"
+      },
+      open: true,
+      hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin()
     ]
-  },
-  resolve: { extensions: ["*", ".js", ".jsx", ".css", ".less"] },
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "[name].js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new MiniCssExtractPlugin()
-  ]
 };
